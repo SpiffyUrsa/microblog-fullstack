5ca9ea7ec437bf77fa3b9b30b905246576a4c05a
Jonathan Wong <jonathan.a.wong@gmail.com>
2020-10-12 21:29:34 -0700

    Changed title description

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,8 +5,7 @@ function Home() {
   return (
     <main>
       <p>
-        Welcome to <b>Microblog</b>, our innovative site for communicating
-        on the information superhighway.
+        Welcome to <b>Microblog</b>. Stay a while and listen.
       </p>
       <TitleList />
     </main>
