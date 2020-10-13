3b9bc29e148884c7246e30b9dfdbd553bf3808ac
Jonathan Wong <jonathan.a.wong@gmail.com>
2020-10-12 21:28:17 -0700

    reformatted code for a cleaner look

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -7,7 +7,7 @@ import React from 'react';
  *
  */
 
-function Comment({deleteComment, text, id}) {
+function Comment({ deleteComment, text, id }) {
 
   function handleDelete(evt) {
     deleteComment(id);
